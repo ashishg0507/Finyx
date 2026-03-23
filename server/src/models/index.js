3d23f91ee21ccecc222b8dbb9017f7c33c50f633
Akshit900 <akshitkoundal91@gmail.com>
2026-03-23 10:33:17 +0530

    database

---
+++ server/src/models/index.js
@@ -0,0 +1,23 @@
+import { User } from './User.js'
+import { Transaction } from './Transaction.js'
+import { Budget } from './Budget.js'
+import { Goal } from './Goal.js'
+import { GoalContribution } from './GoalContribution.js'
+
+User.hasMany(Transaction, { foreignKey: 'userId', onDelete: 'CASCADE' })
+Transaction.belongsTo(User, { foreignKey: 'userId' })
+
+User.hasMany(Budget, { foreignKey: 'userId', onDelete: 'CASCADE' })
+Budget.belongsTo(User, { foreignKey: 'userId' })
+
+User.hasMany(Goal, { foreignKey: 'userId', onDelete: 'CASCADE' })
+Goal.belongsTo(User, { foreignKey: 'userId' })
+
+Goal.hasMany(GoalContribution, { foreignKey: 'goalId', onDelete: 'CASCADE' })
+GoalContribution.belongsTo(Goal, { foreignKey: 'goalId' })
+
+User.hasMany(GoalContribution, { foreignKey: 'userId', onDelete: 'CASCADE' })
+GoalContribution.belongsTo(User, { foreignKey: 'userId' })
+
+export { User, Transaction, Budget, Goal, GoalContribution }
+
